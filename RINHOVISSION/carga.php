81385af6aed27168b04dde838666262072f77037
Tsukiro2212 <rollsroyce2212@gmail.com>
2024-10-24 18:30:26 -0600

    Add files via upload

--- RINHOVISSION/carga.php
+++ RINHOVISSION/carga.php
@@ -9,52 +9,8 @@ $nombre = isset($_GET['nom']) ? $_GET['nom'] : '';  // Nombre del usuario
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cargastyle.css">
     <title>Cargando...</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #454344;
-            font-family: Arial, sans-serif;
-        }
-
-        .container {
-            text-align: center;
-        }
-
-        .logo {
-            width: 300px;
-            margin-bottom: 20px;
-        }
-
-        .loading-text {
-            font-size: 24px;
-            color: #fff;
-            /* Cambié el color para que resalte más */
-            margin-bottom: 20px;
-        }
-
-        .loading-bar {
-            width: 200px;
-            height: 10px;
-            background-color: #e0e0e0;
-            border-radius: 5px;
-            overflow: hidden;
-            margin: 0 auto;
-            /* Para centrar la barra de progreso */
-        }
-
-        .progress {
-            width: 0%;
-            height: 100%;
-            background: linear-gradient(to right, #ff69b4, #87CEFA, #00008B);
-            transition: width 0.5s ease;
-        }
-    </style>
 </head>
 
 <body>
